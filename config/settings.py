5842582c1fb76fa154a284df1c0fd8920da92a12
shinminju <sinminju73@gmail.com>
2025-05-31 23:12:48 +0900

    -settings.py: 앱 추가 - api, authentication, user 앱 추가

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 앱 추가
+    'api',
+    'authentication',
+    'user'
 ]
 
 MIDDLEWARE = [
