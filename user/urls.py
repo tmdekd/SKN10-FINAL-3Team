023fac040718f485b3e3ebde198ece4f6fac25ff
tmdekd <moon010103@naver.com>
2025-06-10 21:17:32 +0900

    메인페이지로 가는 url 수정

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,9 @@
 # user/urls.py
 from django.urls import path
-from .views import login_page, main_page, profile
+from .views import login_page, profile
 
 # API 엔드포인트 URL 구성
 urlpatterns = [
     path('login/', login_page, name='login-page'), # 로그인 페이지
-    path('main-page/', main_page, name='main-page'), # 메인 페이지
     path('profile/', profile, name='profile'),  # 프로필 페이지
 ]
--- user/views.py
+++ user/views.py
@@ -8,15 +8,6 @@ def login_page(request):
     response.delete_cookie('refresh_token')
     return response
 
-# 메인 페이지 뷰
-def main_page(request):
-    # 사용자 아이디를 통해 사용자 이름 조회 후 main.html에 전달
-    # request.user는 현재 로그인된 사용자 정보를 포함
-    print("[메인 페이지] 진입")
-    # 세션에서 확인하는 방법 -> 에세스 토큰을 열어서 확인
-    # json형태로 값을 넣어야함
-    return render(request, 'main.html')
-
 # 프로필 페이지 뷰
 def profile(request):
     return render(request, 'user/profile.html')
