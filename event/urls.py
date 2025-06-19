6ed1561f2c5ebd416ee25f2943660f03c7736fc8
seunggi-coding <moonsys010103@gmail.com>
2025-06-19 14:30:22 +0900

    압축한 파일을 넣기 위해서 기존 파일들 전부 제거

--- event/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import index, write_event
-
-# http://www.localhost:8000/*
-urlpatterns = [
-    path('', index, name='event-index'),  # http://www.localhost:8000/event/
-    path('write/', write_event, name='event-write'),  # http://www.localhost:8000/event/write/
-]
--- user/urls.py
+++
@@ -1,9 +0,0 @@
-# user/urls.py
-from django.urls import path
-from .views import login_page, profile
-
-# API 엔드포인트 URL 구성
-urlpatterns = [
-    path('', login_page, name='login-page'), # 로그인 페이지
-    path('profile/', profile, name='profile'),  # 프로필 페이지
-]
