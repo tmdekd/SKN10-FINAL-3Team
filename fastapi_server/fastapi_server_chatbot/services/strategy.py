6aed0914edc112694d796f7542809c52ee8f68da
khs2617 <khs980714@gmail.com>
2025-07-04 10:01:50 +0900

    #53: chore: strategy prompt fix

--- fastapi_server/fastapi_server_chatbot/services/strategy.py
+++ fastapi_server/fastapi_server_chatbot/services/strategy.py
@@ -2,6 +2,7 @@
 def get_request_prompt(request):
     if request.client_role == "피고":
         system_prompt = f"""다음 사건에 대한 {request.client_role} 전략을 제시해주세요.
+각 항목은 중복되지 않아야 합니다.
 각 양식마다 줄바꿈을 사용해주세요.
 출력 양식:
 ### 전략 방향성:
@@ -13,6 +14,7 @@ def get_request_prompt(request):
 """
     elif request.client_role == "원고":
         system_prompt = f"""다음 사건에 대한 {request.client_role} 전략을 제시해주세요.
+각 항목은 중복되지 않아야 합니다.
 각 양식마다 줄바꿈을 사용해주세요.
 출력 양식:
 ### 예상 피고 주장:
